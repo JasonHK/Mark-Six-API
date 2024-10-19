9238fd62361519ec39c552c13048cd55b9faa521
Jason Kwok <4410086+JasonHK@users.noreply.github.com>
2024-10-19 18:18:56 +0000

    Removed isValidLanguage

--- src/_commom/isValidLanguage.ts
+++
@@ -1,8 +0,0 @@
-export type Language = "zh-HK" | "en-US";
-
-export const LANGUAGES = ["zh-HK", "en-US"];
-
-export default function isValidLanguage(language: string): language is Language
-{
-    return LANGUAGES.includes(language);
-}
